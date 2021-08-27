a74cc78691183e033338e6dff852ce6982a2795a
ttucker <tracytuckeremail@gmail.com>
2021-08-27 13:22:09 -0500

    Changes made to login component

--- src/Login.js
+++ src/Login.js
@@ -1,61 +1,27 @@
-// import React, { useCallback, useContext } from 'react'
-// import { withRouter, Redirect } from 'react-router'
-// import { auth } from './firebase'
-// import { AuthContext } from './Auth';
+import { useCallback } from 'react'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
-// const Login = ({ history }) => {
-//     const handleLogin = useCallback(
-//         async event => {
-//             event.preventDefault();
-//             const { email, password } = event.target.elements;
-//         try {
-//             await auth
-//             .auth()
-//             .signInWithEmailAndPassword(email.value, password.value);
-//             history.push("/");
-//         } catch (error) {
-//             alert(error);
-//         }
-//     },
-//     [history]
-// );
+export const Login = () => {
+  const handleSubmit = useCallback(async e => {
+    e.preventDefault()
 
-// const { currentUser } = useContext(AuthContext);
+    const { email, password } = e.target.elements
+    const auth = getAuth()
+    try {
+      await signInWithEmailAndPassword(auth, email.value, password.value)
+    } catch (e) {
+      alert(e.message)
+    }
+  }, [])
 
-// if (currentUser) {
-//     return <Redirect to="/" />
-// }
-
-//     return (
-//         <div>
-//             <h1>Login Here:</h1>
-//             <form onSubmit={handleLogin}>
-//                 <label>
-//                     Email
-//                     <input name="email" type="email" placeholder="Email goes here..." />
-//                 </label>
-//                 <label>
-//                     Password
-//                     <input name="password" type="password" placeholder="Password goes here..." />
-//                 </label>
-//                 <button type="submit">Log in</button>
-//             </form>
-//         </div>
-//     );
-// };
-
-// export default withRouter(Login);
-
-
-
-import React from 'react'
-
-function Login() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input name="email" placeholder="email" type="email" />
+        <input name="password" placeholder="password" type="password" />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  )
 }
-
-export default Login
