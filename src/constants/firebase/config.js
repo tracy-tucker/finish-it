4da138ea603b6efb50b4e6b9a95df56f0c7e5d73
ttucker <tracytuckeremail@gmail.com>
2021-09-04 08:39:46 -0500

    Initializing Firebase in project

--- src/constants/firebase/config.js
+++ src/constants/firebase/config.js
@@ -35,3 +35,20 @@
 // const db = getFirestore()
 
 // export { db }
+
+import Firebase from 'firebase'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCH09L4i49i-xi6ZBF6ps5GscgNOTfXabM",
+    authDomain: "grindstone-c0642.firebaseapp.com",
+    projectId: "grindstone-c0642",
+    storageBucket: "grindstone-c0642.appspot.com",
+    messagingSenderId: "488452110279",
+    appId: "1:488452110279:web:c38ca051cbc653b36e8be2",
+    measurementId: "G-QH72MGPVLK"
+  };
+
+const firebase = Firebase.initializeApp(firebaseConfig)
+
+export { firebase };
