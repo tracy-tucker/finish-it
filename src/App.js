a600b8e1ef0d2b32a6886d8f01e8df6e53875eed
ttucker <tracytuckeremail@gmail.com>
2021-09-04 06:11:35 -0500

    Reorganizing component file structures

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@
 // Add "Loading" message
 
 import React from 'react'
-import Navbar from './Navbar'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './components/styles.css'
+import './styles.css'
 
 function Navbar() {
     return (
