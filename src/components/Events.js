9a8c20e339ecbbbb7f7973b0be2fcde455caea29
ttucker <tracytuckeremail@gmail.com>
2021-09-07 13:20:03 -0500

    Creating useEffect hook to gather data from Firestore

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { db } from '../firebase/config'
 
 const Events = () => {
+
+    const [events, setEvents] = useState([]);
     return (
         <div>
             <h1>EVENTS</h1>
             <p>... list of events</p>
+            {storedEvents.map((event) => {
+                <p>{event.title}</p>
+            })}
+            
         </div>
     )
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,43 +1,6 @@
-// import { getAuth, onAuthStateChanged } from '@firebase/auth'
-// import { initializeApp } from 'firebase/app'
-// import { getFirestore } from 'firebase/firestore'
-// import { useState, useEffect, useContext, createContext } from 'react'
-
-// export const firebaseApp = initializeApp({
-//   apiKey: "AIzaSyCH09L4i49i-xi6ZBF6ps5GscgNOTfXabM",
-//   authDomain: "grindstone-c0642.firebaseapp.com",
-//   projectId: "grindstone-c0642",
-//   storageBucket: "grindstone-c0642.appspot.com",
-//   messagingSenderId: "488452110279",
-//   appId: "1:488452110279:web:c38ca051cbc653b36e8be2",
-//   measurementId: "G-QH72MGPVLK"
-// })
-
-// // Holds the state of the current user signed in.
-// export const AuthContext = createContext()
-
-// export const AuthContextProvider = props => {
-//   const [user, setUser] = useState()
-//   const [error, setError] = useState()
-
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(getAuth(), setUser, setError)
-//     return () => unsubscribe()
-//   }, [])
-//   return <AuthContext.Provider value={{ user, error }} {...props} />
-// }
-
-// export const useAuthState = () => {
-//   const auth = useContext(AuthContext)
-//   return { ...auth, isAuthenticated: auth.user != null }
-// }
-
-// const db = getFirestore()
-
-// export { db }
-
 import Firebase from 'firebase'
 import 'firebase/auth'
+import 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCH09L4i49i-xi6ZBF6ps5GscgNOTfXabM",
@@ -51,4 +14,6 @@ const firebaseConfig = {
 
 const firebase = Firebase.initializeApp(firebaseConfig)
 
-export { firebase };
+const db = firebase.firestore();
+
+export { firebase, db };
