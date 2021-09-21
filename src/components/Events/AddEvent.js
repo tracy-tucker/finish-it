a3a2030f862a96ec4f7abfb8a293c1b00e312ff6
ttucker <tracytuckeremail@gmail.com>
2021-09-21 14:53:33 -0500

    Adding AddEvent for modal

---
+++ src/components/Events/AddEvent.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Form } from 'react-bootstrap'
+
+const AddEvent = () => {
+    return (
+        <div>
+            <Form>
+                <Form.Group>
+                    <Form.Control
+                        type="text"
+                        placholder="Title *"
+                        required
+                    >
+                    </Form.Control>
+                    <Form.Control
+                        type="date"
+                        placholder="Date *"
+                        required
+                    >
+                    </Form.Control>
+                    <Form.Control
+                        as="texareat"
+                        placholder="Description *"
+                        required
+                        rows={3}
+                    >
+                    </Form.Control>
+                </Form.Group>
+                <Button variant="success" type="submit" block>
+                    Add Event
+                </Button>
+            </Form>
+        </div>
+    )
+}
+
+export default AddEvent
