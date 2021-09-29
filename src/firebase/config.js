98553f750583a0f0067ffdfe7ed61f72fe1479bb
ttucker <tracytuckeremail@gmail.com>
2021-09-29 18:04:40 -0500

    Correction auth key

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCH09L4i49i-xi6ZBF6ps5GscgNOTfXabM",
-    authDomain: "grindstone-c0642.firebaseapp.com",
-    projectId: "grindstone-c0642",
-    storageBucket: "grindstone-c0642.appspot.com",
-    messagingSenderId: "488452110279",
-    appId: "1:488452110279:web:c38ca051cbc653b36e8be2",
-    measurementId: "G-QH72MGPVLK"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
 
 const firebase = Firebase.initializeApp(firebaseConfig)
