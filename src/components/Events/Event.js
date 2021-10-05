91e9b9cdf55318f010d3ee540cf16b3786fdaa6a
ttucker <tracytuckeremail@gmail.com>
2021-10-05 13:08:05 -0500

    Adding DELETE and EDIT buttons

--- src/components/Events/Event.js
+++ src/components/Events/Event.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 const Event = React.memo(({event}) => {
     // const {key, title, date, description} = event
@@ -9,6 +10,8 @@ const Event = React.memo(({event}) => {
             <h2>{event.title}</h2>
             <h3>{event.date}</h3>
             <p>{event.description}</p>
+            <Button className="btn btn-danger">DELETE</Button>
+            <Button className="btn btn-primary">EDIT</Button>
         </div>
     ) 
 })
