691c41e2a1676b85d4ebf69cd2b9d9e827282d93
ttucker <tracytuckeremail@gmail.com>
2021-10-06 13:07:41 -0500

    Adding deleteEvent to EventContext

--- src/components/Events/Event.js
+++ src/components/Events/Event.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Button } from 'react-bootstrap'
+import { EventContext } from '../../context/EventsContext'
 
 const Event = React.memo(({event}) => {
     // const {key, title, date, description} = event
     console.log("I am inside the Event comp")
+
+    const {deleteEvent} = useContext(EventContext)
 
     return (
         <div className="Event" key={event.id}>
             <h2>{event.title}</h2>
             <h3>{event.date}</h3>
             <p>{event.description}</p>
-            <Button className="btn btn-danger">DELETE</Button>
+            <Button onClick={() => deleteEvent(event.id)} className="btn btn-danger">DELETE</Button>
             <Button className="btn btn-primary">EDIT</Button>
         </div>
     ) 
