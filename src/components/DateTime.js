cd758df967303cf27fc3cf7091e059007234cea8
ttucker <tracytuckeremail@gmail.com>
2021-10-15 08:04:55 -0500

    Adding current date to the Dashboard comp

---
+++ src/components/DateTime.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const DateTime = () => {
+    let dateTime = new Date()
+
+    console.log(dateTime)
+
+    return (
+        <div>
+           Date and Time
+        </div>
+    )
+}
+
+export default DateTime
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,9 +2,15 @@ import React from 'react'
 import EventsReadList from '../components/events/EventsReadList'
 
 const Dashboard = () => {
+    
+    const today = new Date().toLocaleDateString()
+
+    console.log(today)
+
     return (
         <div>
             <h1>Welcome | FINISH IT Dashboard</h1>
+            <p>TODAY'S DATE: {today}</p>
            <EventsReadList />
         </div>
     )
