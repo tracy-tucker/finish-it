c9aa77dc0e5c94058f87bd1e02c313fb21cfc62b
ttucker <tracytuckeremail@gmail.com>
2021-10-21 19:14:02 -0500

    Attempting to feed initial data over to task board

---
+++ src/components/task board/TaskBoard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {InitialData} from './InitialData'
+
+const TaskBoard = () => {
+    return (
+        <div>
+            {InitialData.map(column => (
+                const column = 
+            ))}
+        </div>
+    )
+}
+
+export default TaskBoard
