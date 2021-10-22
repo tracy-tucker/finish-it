45147327eeed1d0beb91529ec30b0e5f40c454e0
ttucker <tracytuckeremail@gmail.com>
2021-10-21 19:00:35 -0500

    Creating initial data for task board

---
+++ src/components/task board/InitialData.js
@@ -0,0 +1,8 @@
+const InitialData = {
+    tasks: {
+        'task-1': {id: 'task-1', content: 'Take out the garbage'},
+        'task-2': {id: 'task-2', content: 'Watch my favorite show'},
+        'task-3': {id: 'task-3', content: 'Charge my phone'},
+        'task-4': {id: 'task-4', content: 'Cook dinner'}
+    }
+}
