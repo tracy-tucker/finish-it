35f0b0067c519deaba38af73a0c8b21afe35e39b
ttucker <tracytuckeremail@gmail.com>
2021-10-25 19:20:39 -0500

    Adding color to the draggable task to differentiate on drag

--- src/components/task board/Task.js
+++ src/components/task board/Task.js
@@ -7,6 +7,7 @@ border: 1px solid lightgray;
 border-radius: 2px;
 padding: 8px;
 margin-bottom: 8px;
+background-color: ${(props) => (props.isDragging ? 'lightblue' : 'white')}
 `;
 
 const Task = ({task, index}) => {
@@ -17,6 +18,7 @@ const Task = ({task, index}) => {
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                     ref={provided.innerRef}
+                    isDragging={snapshot.isDragging}
                 >
                     {task.content}
                 </Container>
