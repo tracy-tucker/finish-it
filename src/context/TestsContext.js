af5cb78e580162bec2d02a970139388951b1836b
ttucker <tracytuckeremail@gmail.com>
2021-11-01 13:10:51 -0500

    Creating dummy contextt provider to test Firestore queries

---
+++ src/context/TestsContext.js
@@ -0,0 +1,10 @@
+import React, { createContext, useState, useEffect } from 'react'
+import { db } from '../firebase/config'
+
+export const TestContext = createContext();
+
+const TestContextProvider = (props) => {
+    //
+}
+
+export default TestContextProvider
