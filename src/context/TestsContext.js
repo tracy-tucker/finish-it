d74c5eb905dfb44dd6bf3f81dfb6a7c0871d29ad
ttucker <tracytuckeremail@gmail.com>
2021-11-01 13:19:26 -0500

    Initializing request to database for tasks

--- src/context/TestsContext.js
+++ src/context/TestsContext.js
@@ -4,7 +4,24 @@ import { db } from '../firebase/config'
 export const TestContext = createContext();
 
 const TestContextProvider = (props) => {
-    //
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        const subscriber = db
+        .collection('tasks').onSnapshot(querySnapshot => {
+            setTasks(querySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }))) // end SetTasks
+        }) // end collection call
+        return () => subscriber()
+    }, [])
+
+    return (
+        <TestContext.Provider value={tasks}>
+            {props.children}
+        </TestContext.Provider>
+    )
 }
 
 export default TestContextProvider
